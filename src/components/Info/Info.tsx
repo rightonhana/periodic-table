0f22a0b84755495fd2675c2c0878aae88272f910
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-07-31 14:21:51 -0700

    :sparkles: add Info component

---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from "react";
+import {css} from "emotion";
+import InfoProps from "./InfoProps";
+import Title from "../Title";
+import Value from "../Value";
+
+export const Info: FC<InfoProps> = ({title = "", value = "", ...props}) => <div className={css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: inherit;
+  margin: 1rem 0;
+`
+} {...props}>
+      <Title title={title}/>
+      <Value value={value}/>
+</div>
+
+export default Info;
---
+++ src/components/Info/InfoProps.ts
@@ -0,0 +1,8 @@
+export interface InfoProps {
+    /** Title to render */
+    title: string;
+    /** Value to render */
+    value: string;
+}
+
+export default InfoProps;
