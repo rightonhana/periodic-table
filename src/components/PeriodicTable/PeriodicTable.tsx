d7dde235531ac09f2c65cbfc4eb341e176680cf3
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-01 13:16:26 -0700

    :sparkles: add when util

--- src/components/PeriodicTable/PeriodicTable.tsx
+++ src/components/PeriodicTable/PeriodicTable.tsx
@@ -2,16 +2,21 @@ import React, { FC, useState } from 'react';
 import { ElementsData } from '../../types/ElementsData';
 import ElementInfo from '../ElementInfo';
 import Table from '../Table';
+import when from "../../utils/when";
 import { elementsData } from '../../const/elementsData';
 
 export const PeriodicTable: FC<{}> = () => {
-    const [state, setState] = useState<ElementsData>(elementsData[0]);
+    const [elementShowing, setElementShowing] = useState<ElementsData>();
 
-    const setInfo = (element: ElementsData) => setState(element);
+    const setElement = (element: ElementsData) => setElementShowing(element);
 
     return (<div>
-        <Table width={10} height={10} onElementClick={setInfo} />
-        <ElementInfo element={state}/>
+        <Table width={10} height={10} onElementClick={setElement} />
+        { when(
+            elementShowing !== undefined,
+            <ElementInfo element={elementShowing ? elementShowing: elementsData[0]}/>,
+            <></>
+        ) }
     </div>)
 }
 
