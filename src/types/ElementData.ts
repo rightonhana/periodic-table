bb9e550233a7d498ac348606d1141897387a7948
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-03 15:32:37 -0700

    :bug: fix types :pencil2: change ElementData name :sparkles: update ElementData type

---
+++ src/types/ElementData.ts
@@ -0,0 +1,25 @@
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+import States from "../enums/States";
+import typeColor from "../const/typeColor";
+
+export type ElementData = {
+    acidBaseProperties: AcidBaseProperties;
+    atomicMass: number;
+    atomicNumber: number;
+    boilingTemperature: number;
+    commonOxidationStates: number;
+    crystalStructure: CrystalStructures;
+    electronConfiguration: string;
+    fusionTemperature: number;
+    group: number;
+    meltingTemperature: number;
+    metalMetalloidNonmetal: keyof typeof typeColor;
+    name: string;
+    period: number;
+    special: boolean;
+    state: States;
+    symbol: string;
+}
+
+export default ElementData;
