9fa24b67ca591e9842d94c60223b1d77567ffb5f
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-03 15:37:04 -0700

    :sparkles: add hydrogen element data

---
+++ src/elements/hydrogen.ts
@@ -0,0 +1,25 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const hydrogen: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID_BASE,
+    atomicMass: 1.00794,
+    atomicNumber: 1,
+    boilingTemperature: -252.87,
+    commonOxidationStates: 1,
+    crystalStructure: CrystalStructures.HEX,
+    electronConfiguration: "1s1",
+    fusionTemperature: -259.34,
+    group: 1,
+    meltingTemperature: -259.16,
+    metalMetalloidNonmetal: "reactive nonmetal",
+    name: "hydrogen",
+    period: 0,
+    special: false,
+    state: States.GAS,
+    symbol: "h"
+};
+
+export default hydrogen;
