db84063bc0b4d41a84e07f8f900b33d76f9f12d4
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-03 14:53:45 -0700

    :sparkles: add AcidBaseProperty component

---
+++ src/components/AcidBaseProperty/AcidBasePropertyProps.ts
@@ -0,0 +1,10 @@
+export interface AcidBasePropertyProps {
+    /** Color to render */
+    color: string;
+    /** Secondary color to render */
+    secondaryColor?: string;
+    /** Stroke value */
+    stroke?: number;
+}
+
+export default AcidBasePropertyProps;
---
+++ src/components/AcidBaseProperty/index.ts
@@ -0,0 +1,1 @@
+export { default, AcidBaseProperty } from "./AcidBaseProperty";
