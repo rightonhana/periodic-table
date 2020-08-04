d12b7de858f3a6c6c3a2b91bfa2235a007d4e2cd
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-04 09:23:32 -0700

    :sparkles: add carbon element data

---
+++ src/elements/carbon.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const carbon: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 12.011,
+    atomicNumber: 6,
+    boilingTemperature: 3727,
+    commonOxidationStates: [2, 4],
+    crystalStructure: CrystalStructures.HEX,
+    electronConfiguration: "1s2 2s2 2p2",
+    fusionTemperature: 3652,
+    group: 0,
+    metalMetalloidNonmetal: "reactive nonmetal",
+    name: "carbon",
+    period: 0,
+    special: false,
+    state: States.SOLID,
+    symbol: "c"
+};
+
+export default carbon;
