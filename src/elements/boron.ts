11f9907e33dbe200c2ef6c4f4f76c287a60fb16a
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-04 09:22:46 -0700

    :sparkles: add boron element data

---
+++ src/elements/boron.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const boron: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 10.811,
+    atomicNumber: 5,
+    boilingTemperature: 2550,
+    commonOxidationStates: [3],
+    crystalStructure: CrystalStructures.RHO,
+    electronConfiguration: "1s2 2s2 2p1",
+    fusionTemperature: 2079,
+    group: 0,
+    metalMetalloidNonmetal: "metalloid",
+    name: "boron",
+    period: 0,
+    special: false,
+    state: States.SOLID,
+    symbol: "b"
+};
+
+export default boron;
