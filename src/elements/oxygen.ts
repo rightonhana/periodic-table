ce2f06b436fa5c843dc3b6e5db82609c36ca92e2
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-04 09:25:51 -0700

    :sparkles: add oxygen element data

---
+++ src/elements/oxygen.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const oxygen: ElementData = {
+    acidBaseProperties: AcidBaseProperties.NEUTRAL,
+    atomicMass: 15.9994,
+    atomicNumber: 8,
+    boilingTemperature: -182.962,
+    commonOxidationStates: [-2],
+    crystalStructure: CrystalStructures.SC,
+    electronConfiguration: "1s2 2s2 2p4",
+    fusionTemperature: -218.4,
+    group: 0,
+    metalMetalloidNonmetal: "reactive nonmetal",
+    name: "oxygen",
+    period: 0,
+    special: false,
+    state: States.GAS,
+    symbol: "o",
+};
+
+export default oxygen;
