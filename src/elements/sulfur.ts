1101fe78f510d5c8f8a36ce2b6ef9ed04af2b6c2
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-05 09:27:37 -0700

    :sparkles: add sulfur element data

---
+++ src/elements/sulfur.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const sulfur: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 32.066,
+    atomicNumber: 16,
+    boilingTemperature: 444.674,
+    commonOxidationStates: [-2, 2, 4, 6],
+    crystalStructure: CrystalStructures.ORTH,
+    electronConfiguration: "[Ne] 3s2 3p4",
+    fusionTemperature: 112.8,
+    group: 16,
+    metalMetalloidNonmetal: "reactive nonmetal",
+    name: "sulfur",
+    period: 3,
+    special: false,
+    state: States.SOLID,
+    symbol: "S"
+};
+
+export default sulfur;
