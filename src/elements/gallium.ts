b66d3018931a9fa210de63fcf0a3d494f8804156
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-05 15:47:41 -0700

    :sparkles: add gallium element data

---
+++ src/elements/gallium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const gallium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID_BASE,
+    atomicMass: 69.723,
+    atomicNumber: 31,
+    boilingTemperature: 2403,
+    commonOxidationStates: [3],
+    crystalStructure: CrystalStructures.ORTH,
+    electronConfiguration: "[Ar] 4s2 3d10 4p1",
+    fusionTemperature: 29.78,
+    group: 13,
+    metalMetalloidNonmetal: "post-​transition metal",
+    name: "gallium",
+    period: 4,
+    special: false,
+    state: States.LIQUID,
+    symbol: "Ga"
+};
+
+export default gallium;
