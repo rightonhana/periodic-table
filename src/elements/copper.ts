3f878280d866df59e0f661b7e3aa75015e238c9d
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-05 15:46:43 -0700

    :sparkles: add copper element data

---
+++ src/elements/copper.ts
@@ -0,0 +1,23 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const copper: ElementData = {
+    acidBaseProperties: AcidBaseProperties.BASE,
+    atomicMass: 63.546,
+    atomicNumber: 29,
+    boilingTemperature: 2567,
+    crystalStructure: CrystalStructures.FCC,
+    electronConfiguration: "[Ar] 4s2 3d9",
+    fusionTemperature: 1083.4,
+    group: 11,
+    metalMetalloidNonmetal: "transition metal",
+    name: "copper",
+    period: 4,
+    special: false,
+    state: States.SOLID,
+    symbol: "Cu"
+};
+
+export default copper;
