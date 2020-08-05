2779f4b45e5d2754f29db3da9223023a3f3adf9e
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-05 09:27:01 -0700

    :sparkles: add phosphorus element data

---
+++ src/elements/phosphorus.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const phosphorus: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 30.973762,
+    atomicNumber: 15,
+    boilingTemperature: 280,
+    commonOxidationStates: [-3, 3, 5],
+    crystalStructure: CrystalStructures.ORTH,
+    electronConfiguration: "[Ne] 3s2 3p3",
+    fusionTemperature: 44.1,
+    group: 15,
+    metalMetalloidNonmetal: "reactive nonmetal",
+    name: "phosphorus",
+    period: 3,
+    special: false,
+    state: States.SOLID,
+    symbol: "P"
+};
+
+export default phosphorus;
