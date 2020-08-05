44086110bfa365f221b94bf7368d572b519d3447
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-05 09:24:44 -0700

    :sparkles: add sodium element data

---
+++ src/elements/sodium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const sodium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.BASE,
+    atomicMass: 22.989768,
+    atomicNumber: 11,
+    boilingTemperature:882.9,
+    commonOxidationStates: [1],
+    crystalStructure: CrystalStructures.BCC,
+    electronConfiguration: "[Ne] 3s1",
+    fusionTemperature: 97.81,
+    group: 1,
+    metalMetalloidNonmetal: "alkali metal",
+    name: "sodium",
+    period: 3,
+    special: false,
+    state: States.SOLID,
+    symbol: "Na"
+};
+
+export default sodium;
