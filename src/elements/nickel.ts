065349fc0e45985dafa0135a1cf7960a90e94103
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-05 15:46:17 -0700

    :sparkles: add nickel element data

---
+++ src/elements/nickel.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const nickel: ElementData = {
+    acidBaseProperties: AcidBaseProperties.BASE,
+    atomicMass: 58.69,
+    atomicNumber: 28,
+    boilingTemperature: 2732,
+    commonOxidationStates: [2, 3],
+    crystalStructure: CrystalStructures.FCC,
+    electronConfiguration: "[Ar] 4s2 3d8",
+    fusionTemperature: 1453,
+    group: 10,
+    metalMetalloidNonmetal: "transition metal",
+    name: "nickel",
+    period: 4,
+    special: false,
+    state: States.SOLID,
+    symbol: "Ni",
+};
+
+export default nickel;
