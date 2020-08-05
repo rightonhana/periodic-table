b8872b8f6007d2dc2c8069bda4880fb087079311
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-05 09:26:30 -0700

    :sparkles: add silicon element data

---
+++ src/elements/silicon.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const silicon: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID_BASE,
+    atomicMass: 28.0855,
+    atomicNumber: 14,
+    boilingTemperature: 2355,
+    commonOxidationStates: [4],
+    crystalStructure: CrystalStructures.DC,
+    electronConfiguration: "[Ne] 3s2 3p2",
+    fusionTemperature: 1410,
+    group: 14,
+    metalMetalloidNonmetal: "metalloid",
+    name: "silicon",
+    period: 3,
+    special: false,
+    state: States.SOLID,
+    symbol: "Si"
+};
+
+export default silicon;
