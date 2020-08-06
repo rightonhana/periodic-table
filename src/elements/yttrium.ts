254e34104de943d5b02248582bd9945a301449b4
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:17:22 -0700

    :sparkles: add yttrium element data

---
+++ src/elements/yttrium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const yttrium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.BASE,
+    atomicMass: 88.90585,
+    atomicNumber: 39,
+    boilingTemperature: 3338,
+    commonOxidationStates: [3],
+    crystalStructure: CrystalStructures.HCP,
+    electronConfiguration: "[Kr] 5s2 4d1",
+    fusionTemperature: 1522,
+    group: 3,
+    metalMetalloidNonmetal: "transition metal",
+    name: "yttrium",
+    period: 5,
+    special: false,
+    state: States.SOLID,
+    symbol: "Y"
+};
+
+export default yttrium;
