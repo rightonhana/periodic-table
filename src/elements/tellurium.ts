a1615038a158cc20e495066ce5a922a7ed274e13
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:49:15 -0700

    :sparkles: add tellurium element data

---
+++ src/elements/tellurium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const tellurium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 127.60,
+    atomicNumber: 52,
+    boilingTemperature: 989.8,
+    commonOxidationStates: [-2, 4, 6],
+    crystalStructure: CrystalStructures.HEX,
+    electronConfiguration: "[Kr] 5s2 4d10 5p4",
+    fusionTemperature: 449.5,
+    group: 16,
+    metalMetalloidNonmetal: "metalloid",
+    name: "tellurium",
+    period: 5,
+    special: false,
+    state: States.SOLID,
+    symbol: "Te"
+};
+
+export default tellurium;
