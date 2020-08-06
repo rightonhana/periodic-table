7b73a672ed14ee769edd05a286665335a7f35f91
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:21:48 -0700

    :sparkles: add antimony element data

---
+++ src/elements/antimony.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const antimony: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 121.75,
+    atomicNumber: 51,
+    boilingTemperature: 1635,
+    commonOxidationStates: [-3, 3, 5],
+    crystalStructure: CrystalStructures.RHO,
+    electronConfiguration: "[Kr] 5s2 4d10 5p3",
+    fusionTemperature: 630.74,
+    group: 15,
+    metalMetalloidNonmetal: "metalloid",
+    name: "antimony",
+    period: 5,
+    special: false,
+    state: States.SOLID,
+    symbol: "Sb"
+};
+
+export default antimony;
