9f1ee0c7bab46e3e7e53a56ef630f13c275ba2db
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:49:57 -0700

    :sparkles: add xenon element data

---
+++ src/elements/xenon.ts
@@ -0,0 +1,23 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const xenon: ElementData = {
+    acidBaseProperties: AcidBaseProperties.NEUTRAL,
+    atomicMass: 131.29,
+    atomicNumber: 54,
+    boilingTemperature: -152.30,
+    crystalStructure: CrystalStructures.FCC,
+    electronConfiguration: "[Kr] 5s2 4d10 5p6",
+    fusionTemperature: -156.6,
+    group: 18,
+    metalMetalloidNonmetal: "noble gas",
+    name: "xenon",
+    period: 5,
+    special: false,
+    state: States.GAS,
+    symbol: "Xe"
+};
+
+export default xenon;
