cad9fa107a3e6bc6a57645e602ab6082de8138e6
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:18:38 -0700

    :sparkles: add molybdenum element data

---
+++ src/elements/molybdenum.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const molybdenum: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 95.94,
+    atomicNumber: 42,
+    boilingTemperature: 4612,
+    commonOxidationStates: [2, 3, 4, 5, 6],
+    crystalStructure: CrystalStructures.BCC,
+    electronConfiguration: "[Kr] 5s2 4d4",
+    fusionTemperature: 2617,
+    group: 6,
+    metalMetalloidNonmetal: "transition metal",
+    name: "molybdenum",
+    period: 5,
+    special: false,
+    state: States.SOLID,
+    symbol: "Mo"
+};
+
+export default molybdenum;
