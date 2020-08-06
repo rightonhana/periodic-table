0a892b4e9e536b1237e5b7181294e60ada1fae8d
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:16:28 -0700

    :sparkles: add rubidium element data

---
+++ src/elements/rubidium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const rubidium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.BASE,
+    atomicMass: 85.4578,
+    atomicNumber: 37,
+    boilingTemperature: 686,
+    commonOxidationStates: [1],
+    crystalStructure: CrystalStructures.BCC,
+    electronConfiguration: "[Kr] 5s1",
+    fusionTemperature: 38.89,
+    group: 1,
+    metalMetalloidNonmetal: "alkali metal",
+    name: "rubidium",
+    period: 5,
+    special: false,
+    state: States.SOLID,
+    symbol: "Rb"
+};
+
+export default rubidium;
