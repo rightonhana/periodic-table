9ca85ee9fdbb4ee833d52aada5d2359bc09e80b1
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:21:00 -0700

    :sparkles: add cadmium element data

---
+++ src/elements/cadmium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const cadmium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.BASE,
+    atomicMass: 112.411,
+    atomicNumber: 48,
+    boilingTemperature: 765,
+    commonOxidationStates: [2],
+    crystalStructure: CrystalStructures.HCP,
+    electronConfiguration: "[Kr] 5s2 4d10",
+    fusionTemperature: 320.9,
+    group: 12,
+    metalMetalloidNonmetal: "post-​transition metal",
+    name: "cadmium",
+    period: 5,
+    special: false,
+    state: States.SOLID,
+    symbol: "Cd"
+};
+
+export default cadmium;
