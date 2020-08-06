7305514911766bae860f1120c1fab0dd3550fe40
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-06 11:19:26 -0700

    :sparkles: add ruthenium element data

---
+++ src/elements/ruthenium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const ruthenium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 101.07,
+    atomicNumber: 44,
+    boilingTemperature: 3900,
+    commonOxidationStates: [2, 3, 4, 6, 8],
+    crystalStructure: CrystalStructures.HCP,
+    electronConfiguration: "[Kr] 5s2 4d6",
+    fusionTemperature: 2310,
+    group: 8,
+    metalMetalloidNonmetal: "transition metal",
+    name: "ruthenium",
+    period: 5,
+    special: false,
+    state: States.SOLID,
+    symbol: "Ru"
+};
+
+export default ruthenium;
