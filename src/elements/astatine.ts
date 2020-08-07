185eb5e3da0fdfa99f495e015ba43cfdd31f29e7
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-07 11:02:05 -0700

    :sparkles: add astatine element data

---
+++ src/elements/astatine.ts
@@ -0,0 +1,20 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+
+export const astatine: ElementData = {
+    atomicMass: 210,
+    atomicNumber: 85,
+    boilingTemperature: 337,
+    commonOxidationStates: [-1, 1, 3, 5, 7],
+    electronConfiguration: "[Xe] 6s2 4f14 5d10 6p5",
+    fusionTemperature: 302,
+    group: 17,
+    metalMetalloidNonmetal: "metalloid",
+    name: "astatine",
+    period: 6,
+    special: false,
+    state: States.SOLID,
+    symbol: "At"
+};
+
+export default astatine;
