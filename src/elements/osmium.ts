acb334e4a826a2e08c74740d14aeb04adeca4486
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-07 10:59:25 -0700

    :sparkles: add osmium element data

---
+++ src/elements/osmium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const osmium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.ACID,
+    atomicMass: 190.2,
+    atomicNumber: 76,
+    boilingTemperature: 5027,
+    commonOxidationStates: [2, 3, 4, 6, 8],
+    crystalStructure: CrystalStructures.HCP,
+    electronConfiguration: "[Xe] 6s2 4f14 5d6",
+    fusionTemperature: 3045,
+    group: 8,
+    metalMetalloidNonmetal: "transition metal",
+    name: "osmium",
+    period: 6,
+    special: false,
+    state: States.SOLID,
+    symbol: "Os"
+};
+
+export default osmium;
