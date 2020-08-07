f25c6fa754bc8ed1c83d665caf12bff69b4727c0
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-07 11:01:00 -0700

    :sparkles: add iridium element data

---
+++ src/elements/iridium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const iridium: ElementData = {
+    acidBaseProperties: AcidBaseProperties.BASE,
+    atomicMass: 192.22,
+    atomicNumber: 77,
+    boilingTemperature: 4130,
+    commonOxidationStates: [2, 3, 4, 6],
+    crystalStructure: CrystalStructures.FCC,
+    electronConfiguration: "[Xe] 6s2 4f14 5d7",
+    fusionTemperature: 2410,
+    group: 9,
+    metalMetalloidNonmetal: "transition metal",
+    name: "iridium",
+    period: 6,
+    special: false,
+    state: States.SOLID,
+    symbol: "Ir"
+};
+
+export default iridium;
