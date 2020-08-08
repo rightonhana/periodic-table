f9d5d56cf737363493320226a4ce12cbd1135ae0
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-08-08 15:05:33 -0700

    :sparkles: add actinium element data

--- src/elements/astatine.ts
+++ src/elements/astatine.ts
@@ -1,11 +1,13 @@
 import ElementData from "../types/ElementData";
 import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
 
 export const astatine: ElementData = {
     atomicMass: 210,
     atomicNumber: 85,
     boilingTemperature: 337,
     commonOxidationStates: [-1, 1, 3, 5, 7],
+    crystalStructure: CrystalStructures.FCC,
     electronConfiguration: "[Xe] 6s2 4f14 5d10 6p5",
     fusionTemperature: 302,
     group: 17,
