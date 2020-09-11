547c6df4a240f929527a32b5000714381434dfd4
Maria Florencia Gonzalez <rightonhana@gmail.com>
2020-09-10 17:22:22 -0700

    :sparkles: add bohrium element data

---
+++ src/elements/bohrium.ts
@@ -0,0 +1,20 @@
+import ElementData from "../types/ElementData";
+import CrystalStructures from "../enums/CrystalStructures";
+import States from "../enums/States";
+
+export const bohrium: ElementData = {
+    atomicMass: 264,
+    atomicNumber: 107,
+    commonOxidationStates: [5, 7],
+    crystalStructure: CrystalStructures.HCP,
+    electronConfiguration: "[Rn] 7s2 5f14 6d5",
+    group: 7,
+    metalMetalloidNonmetal: "transition metal",
+    name: "bohrium",
+    period: 7,
+    special: true,
+    state: States.SOLID,
+    symbol: "Bh"
+};
+
+export default bohrium;
