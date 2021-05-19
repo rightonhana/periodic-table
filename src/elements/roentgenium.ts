369e5e7948a8850744fcc86fb5202c70ca8924e7
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-19 14:25:46 -0700

    :sparkles: add roentgenium element data

---
+++ src/elements/roentgenium.ts
@@ -0,0 +1,20 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const roentgenium: ElementData = {
+	atomicMass: 272,
+	atomicNumber: 111,
+	commonOxidationStates: [-1, 1, 3, 5],
+	crystalStructure: CrystalStructures.BCC,
+	electronConfiguration: "[Rn] 7s2 5f14 6d9",
+	group: 11,
+	metalMetalloidNonmetal: "unknown",
+	name: "roentgenium",
+	period: 7,
+	special: true,
+	state: States.SOLID,
+	symbol: "ds"
+};
+
+export default roentgenium;
