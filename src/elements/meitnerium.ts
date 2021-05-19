830fd1b1aba964040ca0ba5a4210c31be5906cf8
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-19 14:12:13 -0700

    :sparkles: add meitnerium element data

---
+++ src/elements/meitnerium.ts
@@ -0,0 +1,20 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const meitnerium: ElementData = {
+	atomicMass: 278,
+	atomicNumber: 109,
+	commonOxidationStates: [1, 3, 4, 6, 8, 9],
+	crystalStructure: CrystalStructures.FCC,
+	electronConfiguration: "[Rn] 7s2 5f14 6d7",
+	group: 9,
+	metalMetalloidNonmetal: "unknown",
+	name: "meitnerium",
+	period: 7,
+	special: true,
+	state: States.SOLID,
+	symbol: "mt"
+};
+
+export default meitnerium;
