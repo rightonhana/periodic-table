b865d2d0179ece90b1d6051304259537a25d6219
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-20 11:01:44 -0700

    :sparkles: add nihonium element data

---
+++ src/elements/nihonium.ts
@@ -0,0 +1,20 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+
+export const nihonium: ElementData = {
+	atomicMass: 286,
+	atomicNumber: 113,
+	commonOxidationStates: [-1, 1, 3, 5],
+	crystalStructure: CrystalStructures.HCP,
+	electronConfiguration: "[Rn] 7s2 5f14 6d10 7p1",
+	group: 13,
+	metalMetalloidNonmetal: "unknown",
+	name: "nihonium",
+	period: 7,
+	special: true,
+	state: States.SOLID,
+	symbol: "Nh"
+};
+
+export default nihonium;
