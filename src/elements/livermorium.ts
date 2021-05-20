e718051283c34506db88fff1c21f41b990ccc072
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-20 11:04:02 -0700

    :sparkles: add livermorium element data

---
+++ src/elements/livermorium.ts
@@ -0,0 +1,18 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+
+export const livermorium: ElementData = {
+	atomicMass: 293,
+	atomicNumber: 116,
+	commonOxidationStates: [2, 4],
+	electronConfiguration: "[Rn] 7s2 5f14 6d10 7p4",
+	group: 16,
+	metalMetalloidNonmetal: "unknown",
+	name: "livermorium",
+	period: 7,
+	special: true,
+	state: States.SOLID,
+	symbol: "Lv"
+};
+
+export default livermorium;
