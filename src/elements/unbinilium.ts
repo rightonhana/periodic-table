c86a9bcb50d36f473046a63bc903e8c910dbf898
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-20 11:06:29 -0700

    :sparkles: add unbinilium element data

---
+++ src/elements/unbinilium.ts
@@ -0,0 +1,18 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+
+export const unbinilium: ElementData = {
+	atomicMass: 327,
+	atomicNumber: 120,
+	commonOxidationStates: [1, 2, 4],
+	electronConfiguration: "8s2",
+	group: 2,
+	metalMetalloidNonmetal: "unknown",
+	name: "unbinilium",
+	period: 8,
+	special: true,
+	state: States.UNKNOWN,
+	symbol: "Ubn"
+};
+
+export default unbinilium;
