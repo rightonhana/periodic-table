ad25605b810254d264f187ae16f8d880eb232071
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-20 11:04:34 -0700

    :sparkles: add tennessine element data

---
+++ src/elements/tennessine.ts
@@ -0,0 +1,18 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+
+export const tennessine: ElementData = {
+	atomicMass: 294,
+	atomicNumber: 117,
+	commonOxidationStates: [-1, 1, 3, 5],
+	electronConfiguration: "[Rn] 7s2 5f14 6d10 7p5",
+	group: 17,
+	metalMetalloidNonmetal: "unknown",
+	name: "tennessine",
+	period: 7,
+	special: true,
+	state: States.SOLID,
+	symbol: "Ts"
+};
+
+export default tennessine;
