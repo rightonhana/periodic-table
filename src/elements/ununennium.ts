bbf26950f5695250c96e6603f91e2456047f09cd
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-20 11:06:01 -0700

    :sparkles: add ununennium element data

---
+++ src/elements/ununennium.ts
@@ -0,0 +1,18 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+
+export const ununennium: ElementData = {
+	atomicMass: 316,
+	atomicNumber: 119,
+	commonOxidationStates: [1, 3],
+	electronConfiguration: "8s1",
+	group: 1,
+	metalMetalloidNonmetal: "unknown",
+	name: "ununennium",
+	period: 8,
+	special: true,
+	state: States.UNKNOWN,
+	symbol: "Uue"
+};
+
+export default ununennium;
