9fa7a185701d1f14c1f83c5200a4cbcbf900e439
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-20 22:31:30 -0700

    :sparkles: add gadolinium element data

---
+++ src/elements/gadolinium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const gadolinium: ElementData = {
+	acidBaseProperties: AcidBaseProperties.BASE,
+	atomicMass: 157.25,
+	atomicNumber: 64,
+	boilingTemperature: 3273,
+	commonOxidationStates: [3],
+	crystalStructure: CrystalStructures.HEX,
+	electronConfiguration: "[Xe] 6s2 4f8",
+	fusionTemperature: 1313,
+	group: "3 VII",
+	metalMetalloidNonmetal: "lan­thanide",
+	name: "gadolinium",
+	period: 6,
+	special: false,
+	state: States.SOLID,
+	symbol: "Gd"
+};
+
+export default gadolinium;
