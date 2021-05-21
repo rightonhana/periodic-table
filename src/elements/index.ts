b96c5ea2bb80fcc26353bfa3da78a23a406549b7
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-21 09:06:10 -0700

    :sparkles: add elements array

---
+++ src/elements/index.ts
@@ -0,0 +1,245 @@
+import actinium from "./actinium";
+import aluminum from "./aluminum";
+import americium from "./americium";
+import antimony from "./antimony";
+import argon from "./argon";
+import arsenic from "./arsenic";
+import astatine from "./astatine";
+import barium from "./barium";
+import berkelium from "./berkelium";
+import beryllium from "./beryllium";
+import bismuth from "./bismuth";
+import bohrium from "./bohrium";
+import boron from "./boron";
+import bromine from "./bromine";
+import cadmium from "./cadmium";
+import calcium from "./calcium";
+import californium from "./californium";
+import carbon from "./carbon";
+import cerium from "./cerium";
+import cesium from "./cesium";
+import chlorine from "./chlorine";
+import chromium from "./chromium";
+import cobalt from "./cobalt";
+import copernicium from "./copernicium";
+import copper from "./copper";
+import curium from "./curium";
+import darmstadtium from "./darmstadtium";
+import dubnium from "./dubnium";
+import dysprosium from "./dysprosium";
+import einsteinium from "./einsteinium";
+import erbium from "./erbium";
+import europium from "./europium";
+import fermium from "./fermium";
+import flerovium from "./flerovium";
+import fluorine from "./fluorine";
+import francium from "./francium";
+import gadolinium from "./gadolinium";
+import gallium from "./gallium";
+import germanium from "./germanium";
+import gold from "./gold";
+import hafnium from "./hafnium";
+import hassium from "./hassium";
+import helium from "./helium";
+import holmium from "./holmium";
+import hydrogen from "./hydrogen";
+import indium from "./indium";
+import iodine from "./iodine";
+import iridium from "./iridium";
+import iron from "./iron";
+import krypton from "./krypton";
+import lanthanum from "./lanthanum";
+import lawrencium from "./lawrencium";
+import lead from "./lead";
+import lithium from "./lithium";
+import livermorium from "./livermorium";
+import lutetium from "./lutetium";
+import magnesium from "./magnesium";
+import manganese from "./manganese";
+import meitnerium from "./meitnerium";
+import mendelevium from "./mendelevium";
+import mercury from "./mercury";
+import molybdenum from "./molybdenum";
+import moscovium from "./moscovium";
+import neodymium from "./neodymium";
+import neon from "./neon";
+import neptunium from "./neptunium";
+import nickel from "./nickel";
+import nihonium from "./nihonium";
+import niobium from "./niobium";
+import nitrogen from "./nitrogen";
+import nobelium from "./nobelium";
+import oganesson from "./oganesson";
+import osmium from "./osmium";
+import oxygen from "./oxygen";
+import palladium from "./palladium";
+import phosphorus from "./phosphorus";
+import platinum from "./platinum";
+import plutonium from "./plutonium";
+import polonium from "./polonium";
+import potassium from "./potassium";
+import praseodymium from "./praseodymium";
+import promethium from "./promethium";
+import protactinium from "./protactinium";
+import radium from "./radium";
+import radon from "./radon";
+import rhenium from "./rhenium";
+import rhodium from "./rhodium";
+import roentgenium from "./roentgenium";
+import rubidium from "./rubidium";
+import ruthenium from "./ruthenium";
+import rutherfordium from "./rutherfordium";
+import samarium from "./samarium";
+import scandium from "./scandium";
+import seaborgium from "./seaborgium";
+import selenium from "./selenium";
+import silicon from "./silicon";
+import silver from "./silver";
+import sodium from "./sodium";
+import strontium from "./strontium";
+import sulfur from "./sulfur";
+import tantalum from "./tantalum";
+import technetium from "./technetium";
+import tellurium from "./tellurium";
+import tennessine from "./tennessine";
+import terbium from "./terbium";
+import thallium from "./thallium";
+import thorium from "./thorium";
+import thulium from "./thulium";
+import tin from "./tin";
+import titanium from "./titanium";
+import tungsten from "./tungsten";
+import unbinilium from "./unbinilium";
+import ununennium from "./ununennium";
+import uranium from "./uranium";
+import vanadium from "./vanadium";
+import xenon from "./xenon";
+import ytterbium from "./ytterbium";
+import yttrium from "./yttrium";
+import zinc from "./zinc";
+import zirconium from "./zirconium";
+
+export const elements = [
+	hydrogen,
+	helium,
+	lithium,
+	beryllium,
+	boron,
+	carbon,
+	nitrogen,
+	oxygen,
+	fluorine,
+	neon,
+	sodium,
+	magnesium,
+	aluminum,
+	silicon,
+	phosphorus,
+	sulfur,
+	chlorine,
+	argon,
+	potassium,
+	calcium,
+	scandium,
+	titanium,
+	vanadium,
+	chromium,
+	manganese,
+	iron,
+	cobalt,
+	nickel,
+	copper,
+	zinc,
+	gallium,
+	germanium,
+	arsenic,
+	selenium,
+	bromine,
+	krypton,
+	rubidium,
+	strontium,
+	yttrium,
+	zirconium,
+	niobium,
+	molybdenum,
+	technetium,
+	ruthenium,
+	rhodium,
+	palladium,
+	silver,
+	cadmium,
+	indium,
+	tin,
+	antimony,
+	tellurium,
+	iodine,
+	xenon,
+	cesium,
+	barium,
+	lanthanum,
+	hafnium,
+	tantalum,
+	tungsten,
+	rhenium,
+	osmium,
+	iridium,
+	platinum,
+	gold,
+	mercury,
+	thallium,
+	lead,
+	bismuth,
+	polonium,
+	astatine,
+	radon,
+	francium,
+	radium,
+	actinium,
+	rutherfordium,
+	dubnium,
+	seaborgium,
+	bohrium,
+	hassium,
+	meitnerium,
+	darmstadtium,
+	roentgenium,
+	copernicium,
+	nihonium,
+	flerovium,
+	moscovium,
+	livermorium,
+	tennessine,
+	oganesson,
+	ununennium,
+	unbinilium,
+	cerium,
+	praseodymium,
+	neodymium,
+	promethium,
+	samarium,
+	europium,
+	gadolinium,
+	terbium,
+	dysprosium,
+	holmium,
+	erbium,
+	thulium,
+	ytterbium,
+	lutetium,
+	thorium,
+	protactinium,
+	uranium,
+	neptunium,
+	plutonium,
+	americium,
+	curium,
+	berkelium,
+	californium,
+	einsteinium,
+	fermium,
+	mendelevium,
+	nobelium,
+	lawrencium
+];
+
+export default elements;
