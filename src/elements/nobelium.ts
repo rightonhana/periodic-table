c0dcc28796e0c5cd404cf6dd8541340cd2411e13
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-21 09:03:31 -0700

    :sparkles: add nobelium element data

---
+++ src/elements/nobelium.ts
@@ -0,0 +1,23 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const nobelium: ElementData = {
+	acidBaseProperties: AcidBaseProperties.UNKNOWN,
+	atomicMass: 259,
+	atomicNumber: 102,
+	commonOxidationStates: [2, 3],
+	crystalStructure: CrystalStructures.UK,
+	electronConfiguration: "[Rn] 7s2 5f14",
+	fusionTemperature: 827,
+	group: "3 XIII",
+	metalMetalloidNonmetal: "actinide",
+	name: "nobelium",
+	period: 7,
+	special: false,
+	state: States.SOLID,
+	symbol: "No"
+};
+
+export default nobelium;
