010bdc11a24db30ef1bbe8e0acc99cf6c85e39bb
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-21 08:56:36 -0700

    :sparkles: add protactinium element data

---
+++ src/elements/protactinium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const protactinium: ElementData = {
+	acidBaseProperties: AcidBaseProperties.BASE,
+	atomicMass: 231.0359,
+	atomicNumber: 91,
+	boilingTemperature: 4027,
+	commonOxidationStates: [3, 4, 5],
+	crystalStructure: CrystalStructures.TETR,
+	electronConfiguration: "[Rn] 7s2 6d1 5f2",
+	fusionTemperature: 1600,
+	group: "3 II",
+	metalMetalloidNonmetal: "actinide",
+	name: "protactinium",
+	period: 7,
+	special: false,
+	state: States.SOLID,
+	symbol: "Pa"
+};
+
+export default protactinium;
