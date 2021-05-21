2f1cbc5a3d94e9c2128bcc72b5b86208b0e562d1
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-21 09:01:40 -0700

    :sparkles: add californium element data

---
+++ src/elements/californium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const californium: ElementData = {
+	acidBaseProperties: AcidBaseProperties.UNKNOWN,
+	atomicMass: 251,
+	atomicNumber: 98,
+	boilingTemperature: 1472,
+	commonOxidationStates: [3],
+	crystalStructure: CrystalStructures.HEX,
+	electronConfiguration: "[Rn] 7s2 5f10",
+	fusionTemperature: 900,
+	group: "3 IX",
+	metalMetalloidNonmetal: "actinide",
+	name: "californium",
+	period: 7,
+	special: false,
+	state: States.SOLID,
+	symbol: "Cf"
+};
+
+export default californium;
