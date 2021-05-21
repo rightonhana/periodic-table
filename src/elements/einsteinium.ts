d444fdd897da0f0e776632a199c628f16c31b892
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-21 09:02:07 -0700

    :sparkles: add einsteinium element data

---
+++ src/elements/einsteinium.ts
@@ -0,0 +1,24 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const einsteinium: ElementData = {
+	acidBaseProperties: AcidBaseProperties.UNKNOWN,
+	atomicMass: 252,
+	atomicNumber: 99,
+	boilingTemperature: 996,
+	commonOxidationStates: [3],
+	crystalStructure: CrystalStructures.HEX,
+	electronConfiguration: "[Rn] 7s2 5f11",
+	fusionTemperature: 860,
+	group: "3 X",
+	metalMetalloidNonmetal: "actinide",
+	name: "einsteinium",
+	period: 7,
+	special: false,
+	state: States.SOLID,
+	symbol: "Es"
+};
+
+export default einsteinium;
