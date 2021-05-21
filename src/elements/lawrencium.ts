010b6e57f0036b040ef922c5a179ac2ea3544f60
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-21 09:04:14 -0700

    :sparkles: add lawrencium element data

---
+++ src/elements/lawrencium.ts
@@ -0,0 +1,23 @@
+import ElementData from "../types/ElementData";
+import States from "../enums/States";
+import CrystalStructures from "../enums/CrystalStructures";
+import AcidBaseProperties from "../enums/AcidBaseProperties";
+
+export const lawrencium: ElementData = {
+	acidBaseProperties: AcidBaseProperties.UNKNOWN,
+	atomicMass: 260,
+	atomicNumber: 103,
+	commonOxidationStates: [3],
+	crystalStructure: CrystalStructures.FCC,
+	electronConfiguration: "[Rn] 7s2 5f14 6d1",
+	fusionTemperature: 1627,
+	group: "3 XIV",
+	metalMetalloidNonmetal: "actinide",
+	name: "lawrencium",
+	period: 7,
+	special: false,
+	state: States.SOLID,
+	symbol: "Lr"
+};
+
+export default lawrencium;
