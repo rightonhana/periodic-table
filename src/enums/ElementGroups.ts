ef33cf0b68b8e05fed8fed8477a30970f9ec9845
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-23 15:00:57 -0700

    :sparkles: add ElementGroups enum

---
+++ src/enums/ElementGroups.ts
@@ -0,0 +1,14 @@
+export enum ElementGroups {
+	ACTINIDE = "actinide",
+	ALKALI_METAL = "alkali metal",
+	ALKALINE_EARTH_METAL = "alkaline earth metal",
+	LANTHANIDE = "lanthanide",
+	METALLOID = "metalloid",
+	NOBLE_GAS = "noble gas",
+	POST_TRANSITION_METAL = "post transition metal",
+	REACTIVE_NONMETAL = "reactive nonmetal",
+	TRANSITION_METAL = "transition metal",
+	UNKNOWN = "unknown"
+}
+
+export default ElementGroups;
