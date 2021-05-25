89fad3b29e1be8116da7a22f21d104cb494400a7
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-25 14:51:39 -0700

    :art: move styles to css module

--- src/components/Element/Element.tsx
+++ src/components/Element/Element.tsx
@@ -1,28 +1,19 @@
 import {FC} from "react";
 import ElementProps from "./ElementProps";
 import Hexagon from "../Hexagon";
-import {css} from "@emotion/css";
+import styles from "./Element.module.css";
 
-export const Element: FC<ElementProps> = ({name = "", symbol = "", atomicNumber = 0, color = "#fff",  ...props}) => <Hexagon color={color} {...props}>
-	<div className={css`
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		margin: 2rem 0 .5rem 0;
-		`}>
-		<div className={css`
-		font-weight: bold;
-			font-size: 3.5rem;
-		`}>{atomicNumber}</div>
-		<div className={css`
-			font-size: 2.5rem;
-		`}>{name.toUpperCase()}</div>
-	</div>
-	<div className={css`
-		font-size: 8rem;
-		font-weight: bold;
-	`}>{symbol}</div>
-</Hexagon>
+export const Element: FC<ElementProps> = ({
+	name,
+	symbol,
+	atomicNumber,
+	color,
+	...props
+}) => (
+	<Hexagon color={color} name={name} {...props}>
+		<div className={styles.AtomicNumber}>{atomicNumber}</div>
+		<div className={styles.Symbol}>{symbol}</div>
+	</Hexagon>
+);
 
 export default Element;
