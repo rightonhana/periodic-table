9c9eaa3fb0955ace49c1342b7ff0bb4946a6089e
Florencia Gonzalez <rightonhana@gmail.com>
2021-06-09 10:49:23 -0700

    :hammer: refactor HExagon component

--- src/components/Hexagon/Hexagon.tsx
+++ src/components/Hexagon/Hexagon.tsx
@@ -1,58 +1,23 @@
 import {FC} from "react";
-import {css} from "@emotion/css";
 import HexagonProps from "./HexagonProps";
+import styles from "./Hexagon.module.css";
+import CategoryColors from "../../enums/CategoryColors";
 
 export const Hexagon: FC<HexagonProps> = ({
 	color,
 	name,
+	currentCategory,
 	children,
 	...props
-}) => <button className={
-	css`
-		background-color: var(--element);
-		border: none;
-		padding: 0;
-		margin: 0;
-		font: inherit;
-		outline: inherit;
-		height: var(--element-size);
-		width: var(--element-size);
-		color: inherit;
-		display: flex;
-		clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		cursor: pointer;
-		user-select: none;
-		transition: transform 0.2s ease-in-out;
-
-		:hover, :focus {
-			background-color: var(${color});
-			z-index: 1;
-			transform: scale(1.3);
-			div {
-				color: var(${color});
-			}
-		}
-
-	`}
-	title={name}
-	{...props}
->
-	<div className={css`
-		color: var(--element);
-		clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
-		height: var(--inner-element-size);
-		width: var(--inner-element-size);
-		background-color: var(--body);
-
-		:hover, :focus {
-			color: var(${color});
-		}
-	`}>
-		{children}
-	</div>
-</button>
+}) => {
+	var style = {"--color": color, "--element-color": currentCategory ? CategoryColors[currentCategory] : "var(--element)"} as React.CSSProperties;
+	return (
+		<button style={style} className={styles.Hexagon} title={name} {...props}>
+			<div className={styles.InnerHexagon}>
+				{children}
+			</div>
+		</button>
+	);
+}
 
 export default Hexagon;
--- src/components/Hexagon/HexagonProps.ts
+++ src/components/Hexagon/HexagonProps.ts
@@ -1,8 +1,10 @@
 import {HTMLAttributes} from "react";
+import ElementCategories from "../../enums/ElementCategories";
 
 export interface HexagonProps extends HTMLAttributes<HTMLButtonElement> {
 	color: string;
 	name: string;
+	currentCategory: ElementCategories | undefined;
 }
 
 export default HexagonProps;
